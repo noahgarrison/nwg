958e26e9ee6cdae9681d28e1296be98a9e2aa2eb
NoahG1124 <noah.g24@gmail.com>
2013-12-21 09:41:31 -0500

    More work on furnaces, added armor (errored), and work on crafting

---
+++ BrightMod/nwgCraftingHandler.java
@@ -0,0 +1,23 @@
+package nwg.BrightMod;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+import cpw.mods.fml.common.ICraftingHandler;
+
+public class nwgCraftingHandler implements ICraftingHandler{
+
+	@Override
+	public void onCrafting(EntityPlayer player, ItemStack item,
+			IInventory craftMatrix) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSmelting(EntityPlayer player, ItemStack item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
